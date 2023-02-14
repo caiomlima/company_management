6354c773540971feeadcc934aa9c4e794b1f21b0
Caio Moreira Lima <moreiralima.caio@gmail.com>
2023-02-13 23:03:02 -0300

    .

---
+++ company_management_api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace company_management_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+    }
+}
---
+++ company_management_api/Infra/DependenciesConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using company_management_api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace company_management_api.Infra
+{
+    public class DependenciesConfiguration
+    {
+        public static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options => options.UseMySql(configuration["DataBaseConnectionString"], new MySqlServerVersion(new Version(5, 7, 12))));
+                //.ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.DetachedLazyLoadingWarning, RelationalEventId.BoolWithDefaultWarning));
+
+            // Services
+
+
+            // Repositories
+
+
+            // Background Workers & Processors
+
+
+            //
+        }
+    }
+}
